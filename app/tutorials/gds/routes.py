9a501591964a355b872ebf6ed46d11f360268129
Corydon Baylor <cwbaylor1@gmail.com>
2024-11-30 01:27:28 -0500

    pagerank

--- app/tutorials/gds/routes.py
+++ app/tutorials/gds/routes.py
@@ -4,3 +4,7 @@ from . import gds_bp
 @gds_bp.route('/dijkstra')
 def dijkstra():
     return render_template('shortest.html')
+
+@gds_bp.route('/pagerank')
+def pagerank():
+    return render_template('pagerank.html')
